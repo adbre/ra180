8cacc260189debffe9968ce51d5b40f484125e65
Adam Brengesjö <ca.brengesjo@gmail.com>
2014-02-20 21:33:37 +0100

    Run XSockets as Service, and move XSockets client to Html/ folder

---
+++ C42A.Ra180/C42A.Ra180.XSocketsHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace C42A.Ra180.XSocketsHost
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new XSocketServerHost() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ C42A.Ra180/C42A.Ra180.XSocketsHost/XSocketServerHost.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceProcess;
+using XSockets.Core.Common.Socket;
+
+namespace C42A.Ra180.XSocketsHost
+{
+    public partial class XSocketServerHost : ServiceBase
+    {
+        private IXSocketServerContainer _xSocketServer;
+
+        public XSocketServerHost()
+        {
+            InitializeComponent();
+        }
+
+        private static IXSocketServerContainer CreateXSocketServer()
+        {
+            return XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _xSocketServer = CreateXSocketServer();
+            _xSocketServer.StartServers();
+        }
+
+        protected override void OnStop()
+        {
+            var server = _xSocketServer;
+            if (server == null) return;
+            server.StopServers();
+            server.Dispose();
+            _xSocketServer = null;
+        }
+    }
+}
