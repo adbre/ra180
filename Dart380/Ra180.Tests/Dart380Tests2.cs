acda34b70eaa1054f09d662e85ac6cad876aa4d1
Adam Brengesjö <ca.brengesjo@gmail.com>
2015-05-05 19:02:41 +0200

    malin

--- Dart380/Ra180.Tests/Dart380Tests2.cs
+++ Dart380/Ra180.Tests/Dart380Tests2.cs
@@ -64,7 +64,7 @@ namespace Ra180.Tests
         }
 
         [Test]
-        public void KanSändTextmeddelande()
+        public void FMT_KanSkrivaMeddelande()
         {
             _dart.SendKey(Dart380Key.FMT);
             Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FORMAT:         "));
@@ -172,7 +172,48 @@ namespace Ra180.Tests
         [Test]
         public void Isk_BörjarPåTnrOchAvsRad()
         {
-            
+            _dart.SendKey(Dart380Key.FMT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FORMAT:         "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("ROT*NIVÅ"));
+            _dart.SendKey("1");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FORMAT:1        "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("LEDNING "));
+            _dart.SendKey("0");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FORMAT:10       "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("LEDNSBTJ"));
+            _dart.SendKey("0");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FORMAT:100      "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("FRI*TEXT"));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FRI*TEXT*       "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("FRI*TEXT"));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("TILL:           "));
+            _dart.SendKey(Dart380Key.ÄND);
+            _dart.SendKeys("JA");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("TILL:JA         "));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("                "));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("291504*FR:CR    "));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("                "));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("FRÅN:     *U:   "));
+            _dart.SendKey(Dart380Key.ENT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("TEXT:           "));
+            _dart.SendKeys("FÖRBERED RÖ");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("TEXT:FÖRBERED RÖ"));
+            _dart.SendKeys("K 10 LAG");
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("K 10 LAG        "));
+            _dart.SendKey(Dart380Key.SLT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("K 10 LAG        "));
+            _dart.SendKey(Dart380Key.SLT);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("                "));
+
+            _dart.SendKey(Dart380Key.ISK);
+            Assert.That(_dart.LargeDisplay.ToString(), Is.EqualTo("291504*FR:CR    "));
+            Assert.That(_dart.SmallDisplay.ToString(), Is.EqualTo("FRI*TEXT"));
         }
     }
 }
